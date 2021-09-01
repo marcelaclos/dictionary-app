b85f98846d213e4bffe407659367e1d7b0fe0f6d
Marcela Clos <mars@nexiit.com>
2021-09-01 20:08:58 +0200

    I like pasta

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.png";
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+         <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <h1>Welcome to Dictionary App</h1>
+        <p>The best way to explore the world</p>
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Mars Clos</small>
+      </footer>
+      </div>
+      </div>
+  );
+}
