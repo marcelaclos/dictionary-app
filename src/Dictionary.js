186cea8249636acbe84a148165bd7466dd69d0fd
Marcela Clos <mars@nexiit.com>
2021-09-06 11:01:22 +0200

    add files

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -25,8 +25,8 @@ export default function Dictionary(props) {
     axios.get(apiUrl).then(handleDictionResponse);
 
     let pexelsApiKey =
-      "563492ad6f91700001000001fdd29f0808df42bd90c33f42e128fa89";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+      "563492ad6f91700001000001fc2c0428783847048cbc4ac60cda8f66";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=12`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
--- src/Photos.js
+++ src/Photos.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Photos.css";
+import monkey from "./orangutan.png";
 
 export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="Photos">
+        <h2 className="text-center">Images about your search ...</h2>
+        <img src={monkey} className="App-logo img-fluid" alt="logo monkey" />
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
