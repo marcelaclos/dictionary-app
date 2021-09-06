8106a5e0a1a462ef158c199c93f7241ebe741a11
Marcela Clos <mars@nexiit.com>
2021-09-06 14:09:43 +0200

    add files

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,11 +3,27 @@ import "./Phonetic.css";
 
 export default function Phonetic(props) {
   return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-      <span className="text">{props.phonetic.text}</span>
+
+<div className="Phonetic">
+      <div className="row">
+        <div className="col">
+          <p>{props.phonetic.text}</p>
+        </div>
+       
+          <audio controls src={props.phonetic.audio}>
+            Your browser does not support the
+            <code>audio</code> element.
+          </audio>
+       
+      </div>
     </div>
+
+
+    // <div className="Phonetic">
+    //   <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+    //     Listen
+    //   </a>
+    //   <span className="text">{props.phonetic.text}</span>
+    // </div>
   );
 }
