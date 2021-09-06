5d8fcafa29f5efb9ecfcb6778fcbcd16a9d724d3
Marcela Clos <mars@nexiit.com>
2021-09-06 14:10:45 +0200

    add files

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -17,13 +17,5 @@ export default function Phonetic(props) {
        
       </div>
     </div>
-
-
-    // <div className="Phonetic">
-    //   <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-    //     Listen
-    //   </a>
-    //   <span className="text">{props.phonetic.text}</span>
-    // </div>
   );
 }
